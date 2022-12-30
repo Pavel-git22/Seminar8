7b7bbadeec2259766a3cff32da04c777c9c06f57
Павел <p.dolmatov75@gmail.com>
2022-12-30 10:06:51 +0300

    Сделанно первре задание

--- Program.cs
+++ Program.cs
@@ -155,17 +155,17 @@
 //     return newArray;
 // }
 
-// void Show2DArray(int[,] array)
-// {
-//     for (int i = 0; i < array.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < array.GetLength(1); j++)
-//         {
-//             Console.Write(array[i, j] + " ");
-//         }
-//         Console.WriteLine();
-//     }
-// }
+void Show2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
 
 
 // int[,] FindMinNumber(int[,] array)
@@ -214,3 +214,54 @@
 // Console.WriteLine();
 // myArray = FindMinNumber(myArray);
 // Show2DArray(myArray);
+
+
+// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+
+int[,] array54 = {
+{1, 4, 7, 2},
+{5, 9, 2, 3,},
+{8, 4, 2, 4}
+};
+
+
+Show2DArray(array54);
+Console.WriteLine();
+
+
+int[,] organizesColomns(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int Count= 0 ;
+        for (int j = 0; j < array.GetLength(1) - Count+1; j++)
+        {
+          
+            for (int k = 0; k < array.GetLength(1) -1; k++)
+            {                
+                if (array[i, k] < array[i, k+1])
+                {
+                    int temp = array[i, k];
+                    array[i, k] = array[i, k+1];
+                    array[i, k+1] = temp;
+                    Count++;
+                }
+                
+            }
+            
+        }
+    }
+    return array;
+}
+
+Show2DArray(organizesColomns(array54));
